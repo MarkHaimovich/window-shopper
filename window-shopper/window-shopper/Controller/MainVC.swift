9795bddd5334ec96aa9a77672e5d4b2ead47e490
Mark <himovich@gmail.com>
2018-06-16 18:10:29 +0300

    shopping calculator button

--- window-shopper/window-shopper/Controller/MainVC.swift
+++ window-shopper/window-shopper/Controller/MainVC.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var wageTXT: CurrencyTextField!
+    @IBOutlet weak var priceTXT: CurrencyTextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        //target of calcBtn is itself and it open calculator on MainVC wiew for gestures like touchUpInside
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        // when you touch one of these - keyboard apper and you can start using it
+        wageTXT.inputAccessoryView = calcBtn
+        priceTXT.inputAccessoryView = calcBtn
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func calculate(){
+        print("Hey we got here")
     }
-
 
 }
 
